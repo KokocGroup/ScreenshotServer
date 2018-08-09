b82e7685c39baa3a9964bd1431bc8886573fcc17
titov <falgore88@gmail.com>
2018-08-09 19:06:24 +0300

    fix

--- server.js
+++ server.js
@@ -2,16 +2,32 @@ const express = require("express");
 const morgan = require("morgan");
 const routes = require("./src/routes");
 const args = require("args");
+const cluster = require("cluster");
 
 args.option("port", "The port on which the app will be running", 3000);
 args.option("max-instances", "Max browser instances", 5);
 
 const flags = args.parse(process.argv);
 
-const app = express();
-app.use(morgan("tiny"));
-app.use("/", routes);
+if (cluster.isMaster) {
+    const cpuCount = flags.maxInstances || require("os").cpus().length;
 
-app.listen(flags.port, async function() {
-    console.log(`ScreenshotServer listening on port ${flags.port}!`);
-});
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+    cluster.on("exit", function(worker) {
+        console.log(`Worker ${worker.id} died :(`);
+        cluster.fork();
+    });
+    cluster.on("online", worker => {
+        console.log(`Worker ${worker.id} running`);
+    });
+} else {
+    const app = express();
+    app.use(morgan("tiny"));
+    app.use("/", routes);
+
+    app.listen(flags.port, async function() {
+        console.log(`ScreenshotServer listening on port ${flags.port}! Instances: ${flags.maxInstances}`);
+    });
+}
--- src/views/screenshot.js
+++ src/views/screenshot.js
@@ -1,22 +1,71 @@
-const browserQueue = require("../browsers");
+const puppeteer = require("puppeteer");
+const asyncWrap = require("../utils").asyncWrap;
 const _ = require("lodash");
 
-module.exports = async (req, res) => {
+module.exports = asyncWrap(async (req, res) => {
     const target = req.query.url;
     const type = req.query.type || "jpeg";
     const quality = _.toInteger(req.query.quality) || 100;
     const fullPage = req.query.fullPage === "1" ? true : false;
+    const waitUntil = req.query.waitUntil || "domcontentloaded";
+    const timeout = _.toInteger(req.query.timeout) || 5 * 1000;
+    const waitFor = _.toInteger(req.query.waitFor) || 5 * 1000;
+    const width = _.toInteger(req.query.width) || 1280;
+    const height = _.toInteger(req.query.height) || 720;
 
     const task = {
         target,
         type,
         quality,
-        fullPage
+        fullPage,
+        waitUntil,
+        waitFor,
+        timeout
     };
 
-    browserQueue.push(task, result => {
+    console.log("start ", task);
+    let browser = null;
+
+    try {
+        browser = await puppeteer.launch({
+            headless: true,
+            ignoreHTTPSErrors: true,
+            args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--disable-background-networking",
+                "--disable-default-apps",
+                "--disable-extensions",
+                "--disable-sync",
+                "--disable-translate",
+                "--hide-scrollbars",
+                "--metrics-recording-only",
+                "--mute-audio",
+                "--safebrowsing-disable-auto-update"
+            ]
+        });
+
+        const page = await browser.newPage();
+        await page.viewport({
+            width: width,
+            height: height
+        });
+        await page.goto(task.target, { waitUntil: waitUntil, timeout: timeout });
+        await page.waitFor(waitFor);
+        const image = await page.screenshot({
+            fullPage: fullPage,
+            type: type,
+            quality: quality
+        });
         res.status(200);
-        res.contentType('image/jpeg');
-        res.send(result);
-    });
-};
+        res.contentType("image/jpeg");
+        res.send(image);
+    } catch (error) {
+        res.status(400);
+        res.json({
+            error: _.toString(error)
+        });
+    }
+
+    await browser.close();
+});
