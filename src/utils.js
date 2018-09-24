c6c474ebe901addd0843698752673a3b6a785861
Evgeniy Titov <falgore88@gmail.com>
2018-09-24 19:42:36 +0300

    fix

--- src/utils.js
+++ src/utils.js
@@ -10,7 +10,29 @@ const browserPool = createPuppeteerPool({
     puppeteerArgs: {
         headless: true,
         ignoreHTTPSErrors: true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--disable-dev-profile"]
+        args: [
+            "--no-sandbox", 
+            "--disable-setuid-sandbox", 
+            "--disable-dev-shm-usage", 
+            "--disable-dev-profile",
+            "--disable-setuid-sandbox",
+            "--disable-background-networking",
+            "--disable-default-apps",
+            "--ignore-certificate-errors",
+            "--disable-extensions",
+            "--disable-sync",
+            "--disable-translate",
+            "--hide-scrollbars",
+            "--metrics-recording-only",
+            "--mute-audio",
+            "--safebrowsing-disable-auto-update",
+            "--disable-gpu",
+            "--single-process",
+            "--disable-notifications",
+            "--disable-search-geolocation-disclosure",
+            "--disable-web-security",
+            "--disable-dev-profile"
+        ]
     }
 });
 
